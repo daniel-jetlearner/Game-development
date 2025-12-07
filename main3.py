7d55a8f3c0415edb4475426b9f721ef0315345cb
daniel <dannyexcellent44@gmali.com>
2025-12-07 16:02:52 +0000

    tung676

---
+++ main3.py
@@ -0,0 +1,11 @@
+colors = ["red", "blue", "green", "yellow", "purple", "orange"]
+
+index = int(input(" enter index"))
+
+print(colors[index])
+
+prev_num = index-=1
+next_num = index+=1
+
+print(prev_num,index [-1])
+print(next_num,index [+1])
--- project 4/main.py
+++ project 4/main.py
@@ -6,6 +6,10 @@ HEIGHT = 500
 
 
 satellite = []
+start=[]
+end=[]
+gameover = False
+
 
 
 index = 1
@@ -19,35 +23,44 @@ for i in range(8):
 
 
 def draw():
+
+
+
     screen.blit("background",(0,0))
     num = 1
     for s in satellite:
         s.draw()
         screen.draw.text(str(num),color = "white",pos = (s.x+5,s.y+10))
         num+=1
+    for i in range(len(start)):
+        screen.draw.line(start[i],end[i], color = "yellow")
+
+
+    if gameover:
+        screen.draw.text("GAME OVER",pos = (125,250), color= "white", fontsize = 70)
+
+
+
 
 def on_mouse_down(pos):
     global index
     
     if satellite[index].collidepoint(pos):
-        prev_sat.append[index-1] 
+        prev_sat = satellite[index-1]
+        curr_sat = satellite[index]
+        start.append(prev_sat.pos)
+        end.append(curr_sat.pos)
+        index+=1 
 
 
- 
+def timefinish():
+    global gameover
 
+    start.clear()
+    end.clear()
+    satellite.clear()
 
-
-
-
-
-
-
-
-
-
-
-
-
+    gameover = True
 
 
 
@@ -58,4 +71,14 @@ def on_mouse_down(pos):
 
 
 
+
+
+
+
+
+
+
+
+
+clock.schedule(timefinish,2.0)
 pgzrun.go()
